59c9c6e02fc1662eb273decf2c78c2a83d4664f0
Sergio Balcorta <sergio.shearing@gmail.com>
2018-08-09 21:52:44 -0700

    laying out structure for future buildout

--- client_src/src/client/client.c
+++ client_src/src/client/client.c
@@ -1,20 +1,6 @@
 #include "client.h"
 #include <stdio.h>
 
-//  Usage: ./client -n <team> -p <port> [-h <hostname>]
-//      -n team\_name
-//  -p port
-//  -h name of the host, by default it'll be localhost
-
-int     print_usage(void)
-{
-    printf("\nUsage: ./client -n <team> -p <port> [-h <hostname>]\n");
-    printf("-n team\\_name\t");
-    printf("-p port\n");
-    printf("-h name of the host, by default it'll be localhost\n\n");
-    return (0);
-}
-
 void    test_print_flags(t_env *env)
 {
     printf("-n %s\t-p %d\t-h %s\n", env->team_name, env->port, env->hostname);
@@ -26,8 +12,20 @@ int     main(int argc, char **argv)
 
     if (!read_flags(argc, argv, &env))
         return (print_usage());
+
+    init map, events, seed board, and establish users in queue
     
-    test_print_flags(&env);
+    init connection to all users
+
+    while(game still valid and or connections still going)
+    {
+        parse all events and put into queue
+        while (there is a queue)
+        {
+
+        }
+        collect data to broadcast and send out of prospective users
+    }
 
     return (0);
 }
